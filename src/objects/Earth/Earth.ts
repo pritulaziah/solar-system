abd41609d16a03e5a817e14045cf9b2ac2668d2b
pritulaziah <ziahherzen@gmail.com>
2025-03-03 23:31:14 +0500

    refactor: extract Planet base class

--- src/objects/Earth/Earth.ts
+++ src/objects/Earth/Earth.ts
@@ -1,78 +1,22 @@
-import {
-  Scene,
-  CreateSphere,
-  Mesh,
-  Vector3,
-  TransformNode,
-  CreateLines,
-  Color3,
-  LinesMesh,
-} from "@babylonjs/core";
+import { Scene, Color3 } from "@babylonjs/core";
 import { EarthMaterial } from "./EarthMaterial";
+import { Planet } from "../Planet";
 
-export class Earth {
-  private static readonly SEMI_MAJOR_AXIS = 500;
-  private static readonly ECCENTRICITY = 0.0167;
-  private static readonly INCLINATION = 7 * (Math.PI / 180);
-  private static readonly SEMI_MINOR_AXIS =
-    Earth.SEMI_MAJOR_AXIS * Math.sqrt(1 - Earth.ECCENTRICITY ** 2);
-  private static readonly DIAMETER = 1;
-  private static readonly ORBIT_SPEED = 0.01;
-
-  mesh: Mesh;
-  orbitPath: LinesMesh;
-  orbitNode: TransformNode;
-
+export class Earth extends Planet {
   constructor(scene: Scene) {
-    const orbitNode = Earth.createOrbitNode(scene);
-    this.orbitNode = orbitNode;
-    const mesh = Earth.createMesh(scene);
-    mesh.parent = orbitNode;
-    this.mesh = mesh;
-    this.orbitPath = Earth.createOrbitPath(scene);
+    super(scene, "earth", {
+      semiMajorAxis: 500,
+      eccentricity: 0.0167,
+      inclination: 0,
+      diameter: 1,
+      orbitSpeed: 0.01,
+      orbitColor: new Color3(0, 1, 0),
+    });
+    this.mesh.material = new EarthMaterial(scene);
   }
 
   update(elapsedSeconds: number) {
-    const theta = elapsedSeconds * Earth.ORBIT_SPEED;
-    const x = Earth.SEMI_MAJOR_AXIS * Math.cos(theta);
-    const z = Earth.SEMI_MINOR_AXIS * Math.sin(theta);
-    const y = Math.sin(Earth.INCLINATION) * z;
-    this.mesh.position.set(x, y, z);
+    super.update(elapsedSeconds);
     (this.mesh.material as EarthMaterial).update(this.mesh.absolutePosition);
-  }
-
-  private static createOrbitNode(scene: Scene): TransformNode {
-    const orbitNode = new TransformNode("earthOrbit", scene);
-    orbitNode.position = Vector3.Zero();
-
-    return orbitNode;
-  }
-
-  private static createOrbitPath(scene: Scene) {
-    const points: Vector3[] = [];
-    const steps = 100;
-
-    for (let i = 0; i <= steps; i++) {
-      const theta = (i / steps) * Math.PI * 2;
-      const x = Earth.SEMI_MAJOR_AXIS * Math.cos(theta);
-      const z = Earth.SEMI_MINOR_AXIS * Math.sin(theta);
-      const y = Math.sin(Earth.INCLINATION) * z;
-
-      points.push(new Vector3(x, y, z));
-    }
-
-    const orbitPath = CreateLines("earthOrbitPath", { points }, scene);
-    orbitPath.color = new Color3(0, 1, 0);
-
-    return orbitPath;
-  }
-
-  private static createMesh(scene: Scene) {
-    const earth = CreateSphere("earth", { diameter: Earth.DIAMETER, segments: 64 }, scene);
-    earth.rotation.y = Math.PI / 4;
-    const earthMaterial = new EarthMaterial(scene);
-    earth.material = earthMaterial;
-
-    return earth;
   }
 }
---
+++ src/objects/Planet.ts
@@ -0,0 +1,102 @@
+import {
+  Scene,
+  Mesh,
+  TransformNode,
+  LinesMesh,
+  Vector3,
+  CreateLines,
+  Color3,
+  CreateSphere,
+} from "@babylonjs/core";
+
+export type PlanetParams = {
+  orbitColor: Color3;
+  semiMajorAxis: number;
+  eccentricity: number;
+  inclination: number;
+  diameter: number;
+  orbitSpeed: number;
+};
+
+export abstract class Planet {
+  orbitNode: TransformNode;
+  orbitPath: LinesMesh;
+  mesh: Mesh;
+  semiMajorAxis: number;
+  eccentricity: number;
+  inclination: number;
+  diameter: number;
+  orbitSpeed: number;
+
+  constructor(
+    protected scene: Scene,
+    protected name: string,
+    params: PlanetParams
+  ) {
+    this.name = name;
+    this.scene = scene;
+    this.eccentricity = params.eccentricity;
+    this.semiMajorAxis = params.semiMajorAxis;
+    this.inclination = params.inclination * (Math.PI / 180);
+    this.diameter = params.diameter;
+    this.orbitSpeed = params.orbitSpeed;
+    this.orbitNode = this.createOrbitNode();
+    this.mesh = this.createMesh();
+    this.mesh.parent = this.orbitNode;
+    this.orbitPath = this.createOrbitPath(params.orbitColor);
+  }
+
+  get semiMinorAxis() {
+    return this.semiMajorAxis * Math.sqrt(1 - this.eccentricity ** 2);
+  }
+
+  update(elapsedSeconds: number) {
+    const theta = elapsedSeconds * this.orbitSpeed;
+    const x = this.semiMajorAxis * Math.cos(theta);
+    const z = this.semiMinorAxis * Math.sin(theta);
+    const y = Math.sin(this.inclination) * z;
+
+    this.mesh.position.set(x, y, z);
+  }
+
+  private createOrbitNode(): TransformNode {
+    const orbitNode = new TransformNode(`${this.name}Orbit`, this.scene);
+    orbitNode.position = Vector3.Zero();
+
+    return orbitNode;
+  }
+
+  private createOrbitPath(color: Color3): LinesMesh {
+    const points: Vector3[] = [];
+    const steps = 100;
+
+    for (let i = 0; i <= steps; i++) {
+      const theta = (i / steps) * Math.PI * 2;
+      const x = this.semiMajorAxis * Math.cos(theta);
+      const z = this.semiMinorAxis * Math.sin(theta);
+      const y = Math.sin(this.inclination) * z;
+
+      points.push(new Vector3(x, y, z));
+    }
+
+    const orbitPath = CreateLines(
+      `${this.name}OrbitPath`,
+      { points },
+      this.scene
+    );
+    orbitPath.color = color;
+
+    return orbitPath;
+  }
+
+  private createMesh(): Mesh {
+    const planetMesh = CreateSphere(
+      this.name,
+      { diameter: this.diameter, segments: 64 },
+      this.scene
+    );
+    planetMesh.rotation.y = Math.PI / 4;
+
+    return planetMesh;
+  }
+}
