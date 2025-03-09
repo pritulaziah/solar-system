de9b9fd8a2a7f03fc1108b78608f93ed855f8139
pritulaziah <ziahherzen@gmail.com>
2025-03-09 23:26:01 +0500

    refactor: removed SceneManager and moved scene logic to main

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,39 @@
-import { Engine } from "@babylonjs/core";
-import { SceneManager } from '@core/SceneManager';
+import { GUI } from "lil-gui";
+import { GlobalUniforms } from "@core/GlobalUniforms";
+import { CameraController } from "@core/CameraController";
+import { CelestialManager } from "@core/CelestialManager";
+import { PlanetName } from "@objects/constants";
+import { Engine, Scene, Color4 } from "@babylonjs/core";
 import "./style.css";
 
 export class App {
   engine: Engine;
-  sceneManager: SceneManager;
+  globalUniforms: GlobalUniforms;
+  celestialManager: CelestialManager;
+  cameraController: CameraController;
+  params = {
+    backgroundColor: "#1d1f2a",
+    color: "#ff0000",
+    targetPlanet: PlanetName.Earth,
+  };
+  gui: GUI;
+  scene: Scene;
 
   constructor(private canvas: HTMLCanvasElement) {
     this.engine = this.createEngine();
-    this.sceneManager = new SceneManager(this.engine);
+    this.globalUniforms = GlobalUniforms.getInstance(this.engine);
+    this.scene = this.createScene();
+    this.celestialManager = new CelestialManager(this.scene, {
+      referenceDiameter: 1,
+      referenceOrbitRadius: 500,
+      referenceOrbitSpeed: 0.01,
+      referenceRotationSpeed: 0.1,
+    });
+    this.cameraController = new CameraController(
+      this.scene,
+      this.celestialManager.getPlanet(PlanetName.Earth)!.meshNode
+    );
+    this.gui = new GUI({ width: 400 });
   }
 
   createEngine(): Engine {
@@ -22,15 +47,40 @@ export class App {
     return engine;
   }
 
+  createScene(): Scene {
+    const scene = new Scene(this.engine);
+    scene.clearColor = Color4.FromHexString(this.params.backgroundColor);
+
+    return scene;
+  }
+
+  initGui() {
+    const planetNames = Object.values(PlanetName);
+
+    this.gui
+      .add(this.params, "targetPlanet", planetNames)
+      .onChange((newPlanet: PlanetName) => {
+        const planet = this.celestialManager.getPlanet(newPlanet);
+        this.cameraController.setTargetPlanet(planet.meshNode);
+      });
+  }
+
   async init() {
-    this.sceneManager.attachToCanvas(this.canvas);
+    this.initGui();
+    this.cameraController.attachToCanvas(canvas);
     let elapsedSeconds = 0;
 
     this.engine.runRenderLoop(() => {
       const deltaTime = this.engine.getDeltaTime() * 0.001;
       elapsedSeconds += deltaTime;
 
-      this.sceneManager.update(elapsedSeconds);
+      this.globalUniforms.update(
+        elapsedSeconds,
+        this.celestialManager.sun.mesh.position
+      );
+      this.celestialManager.update(elapsedSeconds);
+      this.cameraController.update();
+      this.scene.render();
     });
 
     window.addEventListener("resize", () => this.engine.resize());
